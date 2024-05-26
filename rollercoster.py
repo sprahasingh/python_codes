85c04c7f313ac5def9f1553fd2dea5a3472b3422
Spraha Singh <sprahasiingh@gmail.com>
2024-05-26 19:31:40 +0530

    file modefied

--- rollercoster.py
+++ rollercoster.py
@@ -1,13 +1,21 @@
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+height = int(input("What is your height in cm?\n"))
+bill = 0
 if height > 120:
-  print("You can ride the rollercoaster!")
-  age = int(input("What is your age?"))
-  if age < 12:
-    print("Please pay $5.")
-  elif age <= 18:
-    print("Please pay $7.")
-  else:
-    print("Please pay $12.")
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?\n"))
+    if age < 12:
+        print("Child tickets are $5.")
+        bill = 5
+    elif age <= 18:
+        print("Youth tickets are $7.")
+        bill = 7
+    else:
+        print("Adult tickets are $12.")
+        bill = 12
+    wants_photo = input("Do you want a photo taken? Y or N.\nNOTE: Additional $3 will be charged\n")
+    if wants_photo == "Y":
+      bill+=3
+    print(f"Your final bill is ${bill}")
 else:
-  print("Sorry, you have to grow taller before you can ride.")
+    print("Sorry, you have to grow taller before you can ride.")
