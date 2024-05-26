c2cd7b71f2cc12b4f86ca528f5b53b3d01726fcb
Spraha Singh <sprahasiingh@gmail.com>
2024-05-26 19:15:13 +0530

    new files added

---
+++ bmi.py
@@ -0,0 +1,4 @@
+height = float(input("Enter your height in m\n"))
+weight = float(input("Enter your weight in kg\n"))
+bmi=weight/(height)**2
+print(f"Yout bmi is {int(bmi)}")
---
+++ life_in_weeks.py
@@ -0,0 +1,4 @@
+age = input("Enter your age\n")
+remaining_age=90-int(age)
+weeks=remaining_age*52
+print(f"You have {weeks} weeks left.")
