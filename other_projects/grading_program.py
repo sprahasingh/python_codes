538b309ae3b014486cdfe5b5ee2fb6fe1671b1e1
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:25:13 +0530

    file modified

--- other_projects/grading_program.py
+++ other_projects/grading_program.py
@@ -1,20 +1,20 @@
 student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
 }
 student_grades = {}
 for student in student_scores:
-  score = student_scores[student]
-  grade = student_grades[student]
-  if score >90 and score <= 100 :
-    grade = "Outstanding"
-  elif score >80 and score <= 90 :
-    grade = "Exceeds Expectations"
-  elif score >70 and score <= 80 :
-    grade = "Acceptable"
-  else:
-    grade = "Fail"
+    score = student_scores[student]
+    if 90 < score <= 100:
+        grade = "Outstanding"
+    elif 80 < score <= 90:
+        grade = "Exceeds Expectations"
+    elif 70 < score <= 80:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
+    student_grades[student] = grade
 print(student_grades)
