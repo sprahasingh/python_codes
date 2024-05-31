e35442b78a6466c36043a3f4278dd2ac9563b81d
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:11:48 +0530

    file modified

--- other_projects/banker_roulette.py
+++ other_projects/banker_roulette.py
@@ -1,7 +1,8 @@
 import random
+
 user_input = input("Enter comma-separated names:\n")
 names = user_input.split(",")
 names = [item.strip() for item in names]
 size = len(names)
-index = random.randint(0,size-1)
+index = random.randint(0, size - 1)
 print(f"{names[index]} is going to buy the meal today!")
