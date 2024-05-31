111e54ae3dde4bd17c3d22c7f75d2f7e46428e76
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:29:44 +0530

    file modified

--- other_projects/love_calculator.py
+++ other_projects/love_calculator.py
@@ -5,17 +5,17 @@ name = name1 + name2
 t = name.count("T")
 r = name.count("R")
 u = name.count("U")
-e = name.count("E")
+e1 = name.count("E")
 l = name.count("L")
 o = name.count("O")
 v = name.count("V")
-e = name.count("E")
-num1 = t+r+u+e
-num2 = l+o+v+e
-score = int(str(num1)+str(num2))
-if score < 10 or score >90:
-  print(f"Your score is {score}, you go together like coke and mentos.")
-elif score > 40 and score <50:
-  print(f"Your score is {score}, you are alright together.")
+e2 = name.count("E")
+num1 = t + r + u + e1
+num2 = l + o + v + e2
+score = int(str(num1) + str(num2))
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif 40 < score < 50:
+    print(f"Your score is {score}, you are alright together.")
 else:
-  print(f"Your score is {score}.")
+    print(f"Your score is {score}.")
