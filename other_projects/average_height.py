5ce20e4a4e784ab9aabaa7d0012634e4728f535c
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:11:10 +0530

    file modified

--- other_projects/average_height.py
+++ other_projects/average_height.py
@@ -1,12 +1,12 @@
-student_heights = input("Enter heights seperated by ','\n").split(',')
+student_heights = input("Enter heights separated by ','\n").split(',')
 for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
-sum = 0
-num = 0 
+    student_heights[n] = int(student_heights[n])
+total_sum = 0
+num = 0
 for height in student_heights:
-  sum+=height
+    total_sum += height
 for student in student_heights:
-  num+=1
-print(f"total height = {sum}")
+    num += 1
+print(f"total height = {total_sum}")
 print(f"number of students = {num}")
-print(f"average height = {round(sum/num)}")
+print(f"average height = {round(total_sum / num)}")
