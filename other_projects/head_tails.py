95f2bb7e8de765840465cb26daca662a9762d018
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:25:45 +0530

    file modified

--- other_projects/head_tails.py
+++ other_projects/head_tails.py
@@ -1,8 +1,9 @@
 import random
+
 print("Heads or Tails")
 start = input("Press enter to start\n")
-num = random.randint(0,1)
+num = random.randint(0, 1)
 if num == 1:
-  print("You got Heads")
+    print("You got Heads")
 else:
-  print("You got Tails")
+    print("You got Tails")
