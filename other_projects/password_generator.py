d282743527f01af9c81c3913b1399e5ff05bd5d0
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:31:35 +0530

    file modified

--- other_projects/password_generator.py
+++ other_projects/password_generator.py
@@ -1,32 +1,35 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password=""
-for i in range (0,nr_letters):
+password = ""
+for i in range(0, nr_letters):
     random_letter = random.choice(letters)
-    password+=random_letter
-for i in range (0,nr_symbols):
+    password += random_letter
+for i in range(0, nr_symbols):
     random_symbol = random.choice(symbols)
-    password+=random_symbol
-for i in range (0,nr_numbers):
+    password += random_symbol
+for i in range(0, nr_numbers):
     random_number = random.choice(numbers)
-    password+=random_number
+    password += random_number
 print("In order password:")
 print(password)
 
-pass_set = [0] * (nr_letters+nr_numbers+nr_symbols)
-for i in range (0,(nr_letters+nr_numbers+nr_symbols)):
-    pass_set[i]=password[i]
-password=""
+pass_set = [0] * (nr_letters + nr_numbers + nr_symbols)
+for i in range(0, (nr_letters + nr_numbers + nr_symbols)):
+    pass_set[i] = password[i]
+password = ""
 random.shuffle(pass_set)
 for i in pass_set:
-    password+=str(i)
+    password += str(i)
 print("Out of order password:")
 print(password)
