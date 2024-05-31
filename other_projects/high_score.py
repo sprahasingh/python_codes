e0de84984de478d4a154ae26150451f240fd88f9
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 22:36:55 +0530

    file modified

--- other_projects/high_score.py
+++ other_projects/high_score.py
@@ -1,6 +1,22 @@
-student_scores = input("Enter scores separated by space\n").split()
-for n in range(0, len(student_scores)):
-    student_scores[n] = int(student_scores[n])
+def get_valid_scores():
+    """Prompts the user for scores, validates them, and returns a list of valid scores."""
+    list_student_scores = []
+    while True:
+        scores = input("Enter scores separated by space\n").split()
+        for score_str in scores:
+            try:
+                score = float(score_str)
+                list_student_scores.append(score)
+            except ValueError:
+                print("Invalid score entered. Please enter numbers only.")
+                break
+        if list_student_scores and len(list_student_scores) == len(scores):
+            return list_student_scores
+        else:
+            list_student_scores = []
+
+
+student_scores = get_valid_scores()
 value = 0
 for n in student_scores:
     if n > value:
