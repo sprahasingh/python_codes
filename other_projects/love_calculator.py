e20a5d0770f893630f3cc7d55e18708b69ccca01
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 21:04:08 +0530

    file modified

--- other_projects/love_calculator.py
+++ other_projects/love_calculator.py
@@ -2,16 +2,16 @@ print("The Love Calculator is calculating your score...")
 name1 = input("What is your name?\n").upper()
 name2 = input("What is their name?\n").upper()
 name = name1 + name2
-t = name.count("T")
-r = name.count("R")
-u = name.count("U")
-e1 = name.count("E")
-l = name.count("L")
-o = name.count("O")
-v = name.count("V")
-e2 = name.count("E")
-num1 = t + r + u + e1
-num2 = l + o + v + e2
+t_count = name.count("T")
+r_count = name.count("R")
+u_count = name.count("U")
+e1_count = name.count("E")
+l_count = name.count("L")
+o_count = name.count("O")
+v_count = name.count("V")
+e2_count = name.count("E")
+num1 = t_count + r_count + u_count + e1_count
+num2 = l_count + o_count + v_count + e2_count
 score = int(str(num1) + str(num2))
 if score < 10 or score > 90:
     print(f"Your score is {score}, you go together like coke and mentos.")
