a89e1eda811fb035893b794da5fe1939da8b1311
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:59:28 +0530

    file modified

--- other_projects/rollercoster.py
+++ other_projects/rollercoster.py
@@ -15,7 +15,7 @@ if height > 120:
         bill = 12
     wants_photo = input("Do you want a photo taken? Y or N.\nNOTE: Additional $3 will be charged\n")
     if wants_photo == "Y":
-      bill+=3
+        bill += 3
     print(f"Your final bill is ${bill}")
 else:
     print("Sorry, you have to grow taller before you can ride.")
