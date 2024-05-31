3b4db61b8a758e89bac97c1c3e80118b89e6cf89
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 22:14:29 +0530

    file modified

---
+++ other_projects/treasure_map.py
@@ -0,0 +1,18 @@
+line1 = ["⬜️", "️⬜️", "️⬜️"]
+line2 = ["⬜️", "⬜️", "️⬜️"]
+line3 = ["⬜️️", "⬜️️", "⬜️️"]
+initial_map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+while True:
+    position = input("Where do you want to put the treasure?\ne.g. A1, B3, C2\n").upper()
+    if (len(position) != 2) or not (position[0] in {'A', 'B', 'C'}) or not (position[1] in {'1', '2', '3'}):
+        print("Enter valid input like A1, B3, C2\nNOTE: Size of map is 3*3 (A1-C3)\n")
+    else:
+        break
+pos1 = position[0].upper()
+index2 = int(position[1]) - 1
+char_to_num = {"A": 0, "B": 1, "C": 2}
+index1 = char_to_num[pos1]
+treasure_map = initial_map
+treasure_map[index2][index1] = 'X'
+print(f"{line1}\n{line2}\n{line3}")
