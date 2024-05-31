0d1c23032ea1519dc54ec75749f246270df3fa7b
Spraha Singh <sprahasiingh@gmail.com>
2024-05-31 20:30:33 +0530

    file modified

--- other_projects/paint_area_calculator.py
+++ other_projects/paint_area_calculator.py
@@ -1,7 +1,11 @@
 import math
-def paint_calc(height,width,cover):
-  num = math.ceil((height*width)/cover)
-  print(f"You'll need {num} cans of paint.")
+
+
+def paint_calc(height, width, cover):
+    num = math.ceil((height * width) / cover)
+    print(f"You'll need {num} cans of paint.")
+
+
 test_h = int(input("Enter height of wall\n"))
 test_w = int(input("Enter width of wall\n"))
 coverage = 5
