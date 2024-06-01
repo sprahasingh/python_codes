183bd75e571634ee0d0b932e46a5293737fa0956
Spraha Singh <sprahasiingh@gmail.com>
2024-06-01 15:48:55 +0530

    file modified

--- quiz_game/question_model.py
+++ quiz_game/question_model.py
@@ -8,21 +8,3 @@ class Question:
 
 
 qna_list = [Question(data["text"], data["answer"]) for data in question_data]
-
-
-# question_bank = []
-# for data in question_data:
-#     question_bank.append(Question(data["text"], data["answer"]))
-#
-# new_question_bank = []
-#
-#
-# def get_question():
-#     if quiz.is_question_asked():
-#         for ques in question_bank:
-#             if quiz.question_detail != ques:
-#                 new_question_bank.append(question_bank)
-#     return new_question_bank
-#
-#
-# question_bank = get_question()
--- quiz_game/quiz_brain.py
+++ quiz_game/quiz_brain.py
@@ -4,23 +4,29 @@ import random
 
 class QuizBrain:
     def __init__(self):
-        self.ques_picked = None
-        self.answer = None
-        self.input_answer = None
-        self.ques_asked = []
-        self.ques_number = 0
-        self.score = 0
+        self.ques_picked = None  # contains details of currently asked question
+        self.answer = None  # stores answer of currently asked question
+        self.input_answer = None  # stores answer given by the user
+        self.ques_asked = []  # list that stores all previously asked questions
+        self.ques_number = 0  # stores current question number
+        self.score = 0  # stores current score
 
     def is_question_asked(self, ques):
+        """
+        returns true if the input question is already asked
+        """
         for data in self.ques_asked:
             if data == ques:
                 return True
 
     def ask_question(self):
+        """
+        generates new question and ask that to user
+        """
         if self.ques_number == 0:
             self.ques_picked = random.choice(qna_list)
         else:
-            while self.is_question_asked(self.ques_picked.text):
+            while self.is_question_asked(self.ques_picked.text):  # generate non repetitive question
                 self.ques_picked = random.choice(qna_list)
         self.ques_number += 1
         self.answer = self.ques_picked.answer
@@ -29,11 +35,17 @@ class QuizBrain:
         return self.ques_asked
 
     def is_end(self):
+        """
+        checks if all questions have been asked
+        """
         if len(qna_list) == len(self.ques_asked):
             print(f"\nQUIZ OVER!!!!!\nYour final score is: {self.score}/{self.ques_number}")
             return True
 
     def get_answer(self):
+        """
+        takes user answer as input
+        """
         while True:
             self.input_answer = input().lower()
             if self.input_answer in {'true', 'false'}:
@@ -42,12 +54,12 @@ class QuizBrain:
                 print("Enter valid input: True/False")
 
     def check_answer(self):
+        """
+        checks if the answer given by user is correct
+        """
         if self.input_answer == self.answer:
             self.score += 1
             print("You got it right!")
         else:
             print("That's wrong!")
         print(f"The correct answer was: {self.answer}\nYour current score is: {self.score}/{self.ques_number}")
-
-    def calculate_score(self):
-        pass
