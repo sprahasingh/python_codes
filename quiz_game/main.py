77a12cebc69cea3657190a56d503cc150fee6d13
Spraha Singh <sprahasiingh@gmail.com>
2024-06-01 16:11:32 +0530

    file modified

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -2,7 +2,4 @@ from quiz_brain import QuizBrain
 
 
 quiz = QuizBrain()
-while not quiz.is_end():
-    quiz.ask_question()
-    quiz.get_answer()
-    quiz.check_answer()
+quiz.play_quiz()
--- quiz_game/quiz_brain.py
+++ quiz_game/quiz_brain.py
@@ -4,43 +4,23 @@ import random
 
 class QuizBrain:
     def __init__(self):
-        self.ques_picked = None  # contains details of currently asked question
+        self.remaining_questions = qna_list.copy()  # stores details of questions not asked
+        self.ques_picked = None  # stores details of currently asked question
         self.answer = None  # stores answer of currently asked question
         self.input_answer = None  # stores answer given by the user
-        self.ques_asked = []  # list that stores all previously asked questions
         self.ques_number = 0  # stores current question number
         self.score = 0  # stores current score
-
-    def is_question_asked(self, ques):
-        """
-        returns true if the input question is already asked
-        """
-        for data in self.ques_asked:
-            if data == ques:
-                return True
 
     def ask_question(self):
         """
         generates new question and ask that to user
         """
-        if self.ques_number == 0:
-            self.ques_picked = random.choice(qna_list)
-        else:
-            while self.is_question_asked(self.ques_picked.text):  # generate non repetitive question
-                self.ques_picked = random.choice(qna_list)
+        self.ques_picked = random.choice(self.remaining_questions)
+        self.remaining_questions.remove(self.ques_picked)
         self.ques_number += 1
         self.answer = self.ques_picked.answer
-        self.ques_asked.append(self.ques_picked.text)
+        # self.ques_asked.append(self.ques_picked.text)
         print(f'{self.ques_picked.text} (True/False)?')
-        return self.ques_asked
-
-    def is_end(self):
-        """
-        checks if all questions have been asked
-        """
-        if len(qna_list) == len(self.ques_asked):
-            print(f"\nQUIZ OVER!!!!!\nYour final score is: {self.score}/{self.ques_number}")
-            return True
 
     def get_answer(self):
         """
@@ -63,3 +43,20 @@ class QuizBrain:
         else:
             print("That's wrong!")
         print(f"The correct answer was: {self.answer}\nYour current score is: {self.score}/{self.ques_number}")
+
+    def is_end(self):
+        """
+        checks if all questions have been asked
+        """
+        if not self.remaining_questions:
+            print(f"\nQUIZ OVER!!!!!\nYour final score is: {self.score}/{self.ques_number}")
+            return True
+
+    def play_quiz(self):
+        """
+        steps to play quiz
+        """
+        while not self.is_end():
+            self.ask_question()
+            self.get_answer()
+            self.check_answer()
